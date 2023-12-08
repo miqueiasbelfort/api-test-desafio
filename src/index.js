f2159071bf95de5c4fc2eea87c59cb77c9edd352
Miqueias Kawa <kawasousa1554@gmail.com>
2023-12-08 10:35:17 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
-const {app} = require('./app');
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const user = require("./routes/userRoutes");
+app.use("/", user);
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log("The server is running!")
 });
+
+module.exports = {app};
